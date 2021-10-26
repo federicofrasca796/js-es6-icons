b7cdc41fac9f1fc4a9dd6a682fa1919cf3d9fba7
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-26 17:18:08 +0200

    start milestone 2

---
+++ milestone_2/assets/js/main.js
@@ -0,0 +1,130 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+/* Milestone 1
+Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+*/
+/* Milestone 2
+Coloriamo le icone per tipo
+*/
+
+let iconsTypes = [];
+
+//interate for each array's object
+icons.forEach(icon => {
+	
+	//insert html structure
+	const cardsHTML = `<div class="col card text-center p-3">
+	<i class="${icon.family} ${icon.prefix}${icon.name} ${icon.prefix}2x" data-type="${icon.type}"></i>
+	<div class="icon_name mt-3">${icon.name}</div>
+	</div>
+	`
+	document.querySelector('.row-cols-5').insertAdjacentHTML('beforeend', cardsHTML);
+
+	iconsTypes.push(icon.type);
+});
+
+//select all html elements with data-type attribute which corresponds to one of types listed in icons objects.
+
+//create an array containing every icon type
+console.log(iconsTypes);
+
+
+
+
